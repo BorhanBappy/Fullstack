c201de5d60fa92863e4af93d4f4fcea13f3a938e
Bappy <bappyzx1@gmail.com>
2022-11-29 17:04:30 +0600

    First commit

--- 00-javascript-basic/main.js
+++ 00-javascript-basic/main.js
@@ -1,1 +1,84 @@
-console.log("Hello World")
+// console.log("Hello World Bappy")
+// let age =25
+// age=30
+// console.log(age)
+// const salary=2500
+// // salary=4000
+// // Constant value not change
+// console.log(salary)
+// const name='Bappy'
+// const language="Bangla"
+// const channel=`code`
+
+// const total=0
+// var PI=3.14
+// console.log(PI)
+// const isPrimaryNumber = true
+// const isNewUser= false
+
+// let result
+// console.log(result)
+// const res=undefined
+
+var person={
+
+    firstName:'Borhan Uddin',
+    lastName:'Bappy',
+    age:26
+}
+console.log(person['lastName'])
+// add value an object
+person["mobileNumber"] = 1521788394
+person.village="Banail"
+console.log(person)
+// delete element from object
+delete person.lastName
+console.log(person)
+// empty Object create
+var student = {}
+student.name="bappy"
+console.log(student)
+
+// array
+const oddNumbers=[1,3,5,7,9]
+console.log(oddNumbers[0])// first Element of array
+
+let a=10
+a='Bappy'
+a=true
+console.log(a)// Javascript last element show
+var array
+
+// console.log(2 ** 53-1)
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// let x=Number.MAX_SAFE_INTEGER
+
+// console.log(x+1)
+// console.log(x+10)
+// // create a bigint number
+// let xy= 211111111111111111n
+// console.log(typeof(xy))
+// // BigInt operation 
+// let ab=10n
+// let bc=20n
+// console.log(ab+bc)
+// console.log(bc-ab)
+// // console.log(10+20n) not correct 
+
+// let x=10
+// let y=20
+// console.log(x+y)
+// console.log(x%y)
+// console.log(10 ** 10)// ** means power
+// console.log(5/3)
+// console.log(25/4)
+// console.log(x/y)
+// let x=7
+var y =10
+if(true){
+    var x
+    console.log(x)
+}
+console.log(x)
+
